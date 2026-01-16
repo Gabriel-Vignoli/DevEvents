c4e1dc867b13498df2249ddd66c431131b0fb474
Gabriel-Vignoli <pessoalvignoli123@gmail.com>
2026-01-16 17:57:15 -0300

    feat: create navbar and initial Homepage design

---
+++ next-js-course/project/components/ExploreBtn.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import Image from "next/image";
+
+const ExploreBtn = () => {
+    return (  
+        <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('Hello World')}>
+            <a href="#events">
+                Explore Events
+                <Image src="/icons/arrow-down.svg" alt="Arrow down" width={24} height={24}></Image>
+            </a>
+        </button>
+    );
+}
+ 
+export default ExploreBtn;
